487ad763cff1d03c6bc7695bcb56235acf078121
phichidung <dungpc@sphinx-software.com>
2019-09-23 19:14:23 +0700

    fix project for friendly person

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME
+      host : process.env.MYSQL_HOST,
+      user : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+      database : process.env.MYSQL_DATABASE
     }
   },
 
